4701a51697c69c236bbc0bdde65680f5cc21eaf4
Khanh <khanhtq.b8084@st.usth.edu.vn>
2020-08-03 18:44:07 +0700

    evaluation manager

--- lab/evaluationMng/app.js
+++ lab/evaluationMng/app.js
@@ -6,13 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const bodyParser = require('body-parser');
-const { validateToken } = require('./Libs/Authentication/auth');
+const { dirname } = require('path');
 
 var app = express();
-require ('./Libs/autoload');
-const evaluationMng = require('./lab/evaluationMng/app')
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,14 +18,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(bodyParser.json())
-
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/eva', evaluationMng)
+app.use(express.static(path.join(__dirname, 'node_modules')));
+console.log('dir name: ', __dirname)
 
-app.use('/',validateToken, indexRouter);
-// app.use('/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
