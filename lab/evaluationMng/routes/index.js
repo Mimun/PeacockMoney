1e0705652abee3495e6b65c47461d428f68730dd
Khanh <khanhtq.b8084@st.usth.edu.vn>
2020-08-10 20:23:57 +0700

    added item status module

--- lab/evaluationMng/routes/index.js
+++ lab/evaluationMng/routes/index.js
@@ -1,25 +1,63 @@
 var express = require('express');
 var router = express.Router();
 var Item = require('../models/item')
+var ItemStatus = require('../models/itemStatus')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  Item.find({}, (err, results) => {
+    if (err) throw err
+    console.log('result: ', results)
+    const newResult = results.map(result => {
+      return {
+        infos: result.infos,
+        metadata: result.metadata
+      }
+    })
+    res.render('index', { dbItemObjs: results });
+
+  })
+});
+router.get('/itemStatus', function (req, res, next) {
+  // Item.find({}, (err, results) => {
+  //   if (err) throw err
+  //   console.log('result: ', results)
+  //   const newResult = results.map(result => {
+  //     return {
+  //       infos: result.infos,
+  //       metadata: result.metadata
+  //     }
+  //   })
+  //   res.render('index', { dbItemObjs: results });
+
+  // })
+  res.render('itemStatus', {dbItemObjs: []})
 });
 
-router.post('/postItem', (req, res)=>{
+router.post('/postItem', (req, res) => {
   var itemObjsArray = req.body
-  itemObjsArray.map(itemObj=>{
-    
-    var item = new Item({
-      metadata: itemObj.metadata,
-      infos: itemObj.infos
-    })
+  itemObjsArray.map(itemObj => {
+    if (itemObj._id) {
+      console.log('abc: ', itemObj)
+      Item.findOneAndUpdate({ _id: itemObj._id }, { $set: { "infos": itemObj.infos } }, (err, result) => {
+        if (err) throw err
+        console.log('Updated successfully!')
+      })
 
-    item.save((err, result)=>{
-      if(err) throw err
-      console.log('result: ', result)
-    })
+    } else {
+      var item = new Item({
+        metadata: itemObj.metadata,
+        infos: itemObj.infos
+      })
+
+      item.save((err, result) => {
+        if (err) throw err
+        else {
+          console.log('Saved to db successfully!')
+        }
+      })
+    }
+
 
   })
 })
