b1c63423632625f28b98312cc51499f3fba61a2b
Khanh <khanhtq.b8084@st.usth.edu.vn>
2020-09-25 17:28:29 +0700

    fixed some bugs when importing file

--- js/employeeConfig.js
+++ js/employeeConfig.js
@@ -1,101 +1,103 @@
-export let employeeMetadataSample = {
-  avatar:{
-    "cType" : "image",
-    "dataKor" : "koreanString",
-    "name" : "avatar",
-    "value" : "",
-    "dataVie" : "anhDaiDien"
-  },
-  fullName:{
-    "cType" : "text",
-    "dataKor" : "koreanString",
-    "name" : "fullName",
-    "value" : "",
-    "dataVie" : "hoTen"
-  },
-  customId: {
-    "cType" : "text",
-    "dataKor" : "koreanString",
-    "name" : "customId",
-    "value" : "",
-    "dataVie" : "maNhanVien"
-  },
-  jobTitle: {
-    "cType" : "text",
-    "dataKor" : "koreanString",
-    "name" : "jobTitle",
-    "value" : "",
-    "dataVie" : "chucVu"
-  },
-  address: {
-    "cType" : "text",
-    "dataKor" : "koreanString",
-    "name" : "address",
-    "value" : "",
-    "dataVie" : "diaChi"
-  },
-  idCard: {
-    "cType" : "text",
-    "dataKor" : "koreanString",
-    "name" : "idCard",
-    "value" : "",
-    "dataVie" : "CMND"
-  },
-  providingDate: {
-    "cType" : "date",
-    "dataKor" : "koreanString",
-    "name" : "providingDate",
-    "value" : "",
-    "dataVie" : "ngayCap"
-  },
-  providingPlace: {
-    "cType" : "text",
-    "dataKor" : "koreanString",
-    "name" : "providingPlace",
-    "value" : "",
-    "dataVie" : "noiCap"
-  },
-  phoneNumber: {
-    "cType" : "number",
-    "dataKor" : "koreanString",
-    "name" : "phoneNumber",
-    "value" : "",
-    "dataVie" : "soDienThoai"
-  },
-  email: {
-    "cType" : "email",
-    "dataKor" : "koreanString",
-    "name" : "email",
-    "value" : "",
-    "dataVie" : "email"
-  },
-  note: {
-    "cType" : "text",
-    "dataKor" : "koreanString",
-    "name" : "note",
-    "value" : "",
-    "dataVie" : "ghiChu"
-  },
-  password: {
-    "cType" : "text",
-    "dataKor" : "koreanString",
-    "name" : "password",
-    "value" : "",
-    "dataVie" : "matKhau"
-  },
-  store: {
-    "cType" : "select",
-    "dataKor" : "koreanString",
-    "name" : "store",
-    "value" : "",
-    "dataVie" : "cuaHang"
-  },
-  role: {
-    "cType" : "select",
-    "dataKor" : "koreanString",
-    "name" : "role",
-    "value" : "",
-    "dataVie" : "phanQuyen"
-  }
+export const employeeMetadataSample = () => {
+  return {
+    avatar: {
+      "cType": "image",
+      "dataKor": "koreanString",
+      "name": "avatar",
+      "value": "",
+      "dataVie": "anhDaiDien"
+    },
+    fullName: {
+      "cType": "text",
+      "dataKor": "koreanString",
+      "name": "fullName",
+      "value": "",
+      "dataVie": "hoTen"
+    },
+    customId: {
+      "cType": "text",
+      "dataKor": "koreanString",
+      "name": "customId",
+      "value": "",
+      "dataVie": "maNhanVien"
+    },
+    jobTitle: {
+      "cType": "text",
+      "dataKor": "koreanString",
+      "name": "jobTitle",
+      "value": "",
+      "dataVie": "chucVu"
+    },
+    address: {
+      "cType": "text",
+      "dataKor": "koreanString",
+      "name": "address",
+      "value": "",
+      "dataVie": "diaChi"
+    },
+    idCard: {
+      "cType": "text",
+      "dataKor": "koreanString",
+      "name": "idCard",
+      "value": "",
+      "dataVie": "CMND"
+    },
+    providingDate: {
+      "cType": "date",
+      "dataKor": "koreanString",
+      "name": "providingDate",
+      "value": "",
+      "dataVie": "ngayCap"
+    },
+    providingPlace: {
+      "cType": "text",
+      "dataKor": "koreanString",
+      "name": "providingPlace",
+      "value": "",
+      "dataVie": "noiCap"
+    },
+    phoneNumber: {
+      "cType": "number",
+      "dataKor": "koreanString",
+      "name": "phoneNumber",
+      "value": "",
+      "dataVie": "soDienThoai"
+    },
+    email: {
+      "cType": "email",
+      "dataKor": "koreanString",
+      "name": "email",
+      "value": "",
+      "dataVie": "email"
+    },
+    note: {
+      "cType": "text",
+      "dataKor": "koreanString",
+      "name": "note",
+      "value": "",
+      "dataVie": "ghiChu"
+    },
+    password: {
+      "cType": "text",
+      "dataKor": "koreanString",
+      "name": "password",
+      "value": "",
+      "dataVie": "matKhau"
+    },
+    store: {
+      "cType": "select",
+      "dataKor": "koreanString",
+      "name": "store",
+      "value": "",
+      "dataVie": "cuaHang"
+    },
+    role: {
+      "cType": "select",
+      "dataKor": "koreanString",
+      "name": "role",
+      "value": "",
+      "dataVie": "phanQuyen"
+    }
 
-}
+  }
+} 
